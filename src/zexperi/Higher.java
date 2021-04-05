28fa57656ade05df49cf047fd012ac2265d6ba56
Kellenerskine <kellenae129@gmail.com>
2021-04-05 17:31:34 -0400

    first tests infile

---
+++ src/zexperi/Higher.java
@@ -0,0 +1,37 @@
+package zexperi;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Higher
+ */
+public class Higher {
+    
+    public static void main(String[] args){
+        int a = 25;
+        int b = -25;
+        int c = higher(a,b);
+        System.out.println("The higher of 25 and -25 is: " + c);
+    }
+    /**
+     * @param x
+     * @param y
+     * @return the higher of the two values
+     */
+    public static int higher(int x, int y) {
+        if(x > y) {
+            return x;
+        }else{
+            return y;
+        }
+    }
+
+    @Test
+    public void testHigher() {
+        assertEquals(25, higher(25,25));
+        assertEquals(5, higher(3,5));
+    }
+
+}
