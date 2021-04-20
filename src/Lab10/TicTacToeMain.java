4eb3f0b2255158a306d7547b4d170f6e73a86d21
Kellenerskine <kellenae129@gmail.com>
2021-04-20 14:32:12 -0400

    added the base files for lab10 in

---
+++ src/Lab10/TicTacToeMain.java
@@ -0,0 +1,22 @@
+package Lab10;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+//Do not touch the code below.  It creates an instance of your 
+//TicTacToePanel object and places it into a graphical window.
+
+/**
+ * Creates a graphical window containing a TicTacToePane and
+ * starts the graphical event loop.
+ */
+public class TicTacToeMain {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame window = new JFrame("Tic-Tac-Toe");
+            window.add(new TicTacToePanel());
+            window.pack();
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.setVisible(true);
+        });
+    }
+}
---
+++ src/Lab10/TicTacToePanel.java
@@ -0,0 +1,22 @@
+@SuppressWarnings("serial")
+class TicTacToePanel extends GamePanel {
+    
+    // TODO
+    // Add instance variables as needed
+    
+    // TODO
+    // Add any additional private helper methods you need
+    
+    /** Creates an empty Tic-Tac-Toe board ready for play */
+    public TicTacToePanel() {
+        super(600, 600);  // You need to keep this statement
+        
+        // TODO
+        // Add whatever initialization code you need here
+    }
+    
+    // TODO
+    // Override handleMouseRelease and repaintPanel methods
+    // to make a viable Tic-Tac-Toe game
+    
+}
